ff1259c255fe59bc34cfc7fab507fa394525840e
Takamasa Arakawa <takamasa_arakawa@metaps.com>
2020-11-06 09:17:51 +0900

    update api url

--- config.js
+++ config.js
@@ -1,2 +1,3 @@
-exports.apiUrl = 'https://flow-access-mainnet.portto.io';
+// exports.apiUrl = 'https://flow-access-mainnet.portto.io'; // hosted by Bloct Wallet
+exports.apiUrl = 'https://access-mainnet-beta.onflow.org'; // hosted by Flow
 exports.testUserAddress = '0x4dc8021353a1ecf9';
